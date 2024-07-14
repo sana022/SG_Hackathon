7ffed7456ea5ae85f7e6dda6773e3b3b5ba51565
sana022 <95702980+sana022@users.noreply.github.com>
2024-07-14 10:20:40 +0530

    Add files via upload

--- Backend/models/macroModel.js
+++ Backend/models/macroModel.js
@@ -1,1 +1,12 @@
+const mongoose = require('mongoose');
 
+const macroSchema = new mongoose.Schema({
+  name: String,
+  content: String,
+  documentation: String,
+  transformedCode: String,
+});
+
+const Macro = mongoose.model('Macro', macroSchema);
+
+module.exports = Macro;
