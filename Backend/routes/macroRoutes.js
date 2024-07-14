2360a4360b31b295103c04dbe24fa371837311eb
sana022 <95702980+sana022@users.noreply.github.com>
2024-07-14 10:21:09 +0530

    Add files via upload

--- Backend/routes/macroRoutes.js
+++ Backend/routes/macroRoutes.js
@@ -1,1 +1,9 @@
+// backend/routes/macroRoutes.js
+const express = require('express');
+const router = express.Router();
+const macroController = require('../controllers/macroController');
 
+router.post('/upload', macroController.uploadMacro);
+router.get('/', macroController.getMacros);
+
+module.exports = router;
